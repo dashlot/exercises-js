881184a68ab884bde4b6c3f3cd86b0c1c85b04f2
Dasha Karpova <daschenki@yandex.ru>
2022-12-22 20:24:18 +0400

    feat: create a functionthat return season name by number

--- 2task-season.js
+++ 2task-season.js
@@ -1,5 +1,17 @@
 function getSeasons (monthNumber) {
-    console.log(monthNumber);
+    if ( monthNumber == 1 || monthNumber == 2 || monthNumber == 12 ) {
+        return(`It's winter`);
+    } else if ( 3 >= monthNumber || monthNumber <= 5 ) {
+        return(`It's spring`);
+    } else if ( 6 >= monthNumber || monthNumber <=8 ) {
+        return(`It's summer`);
+    } else if ( 9 >= monthNumber || monthNumber <=11 ) {
+        return(`It's autumn`);
+    } else {
+        return(`It's a mistake`);
+    }
 }
 
-getSeasons(12);
+let number = prompt('Введите номер месяца');
+let getSeasonByNumber = getSeasons(number);
+alert(number + ` — ` + getSeasonByNumber)
